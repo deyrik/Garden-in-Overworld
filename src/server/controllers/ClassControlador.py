a4a2503654dd8a378f003b1c5d74a9f784573bbf
Gabriel de Pádua <gabriel_ferreira100@hotmail.com>
2026-06-10 21:13:07 -0300

    server com middleware
     retira classe tcp inutil
    
    modifica ClasseControladora para lidar com pyro5
    
    main agora cria daemon pra conversar com pyro5

--- src/server/controllers/ClassControlador.py
+++ src/server/controllers/ClassControlador.py
@@ -1,141 +1,37 @@
-import json
+import Pyro5.api
 
+
+# No retorno das funçoes nao preciso preocupar mais em coverter para json para facilitar do outro lado,
+# com o Pyro5 ele vai fazer isso da maneira dele, entao posso so retornar um dicionario mesmo.
+
+@Pyro5.api.expose
+@Pyro5.api.behavior(instance_mode="single") # Garante que todos os jogadores usem a mesma instância da fazenda
 class ControladorFazenda:
-    """Responsável por traduzir JSONs e aplicar as regras de negócio do jogo."""
     def __init__(self, mapa_instancia, gerenciador_instancia):
         self.mapa = mapa_instancia
         self.gerenciador = gerenciador_instancia
 
-    def conecta_jogador(self):
-        """Tenta achar um slot livre e retorna o ID (ou None se cheio)."""
-        return self.gerenciador.adiciona_na_vaga()
+    def conectar(self, nome):
+        """O cliente RMI chama esse método direto para entrar no jogo."""
+        id_jogador = self.gerenciador.adiciona_na_vaga()
+        if id_jogador is not None:
+            self.gerenciador.slots[id_jogador].nick = nome
+            return {"status": "OK", "id": id_jogador, "mensagem": f"BEM_VINDO: {nome}"}
+        return {"status": "ERRO", "mensagem": "A fazenda já atingiu o limite máximo de agricultores (4/4)"}
 
-    def desconecta_jogador(self, id_jogador):
+    def desconectar(self, id_jogador):
         self.gerenciador.remove_da_vaga(id_jogador)
+        return {"status": "OK"}
 
-
-#-------------------------------------------------------------------------------------------------
-    def gera_boas_vindas(self, id_jogador):
-        # Retorna apenas o dicionário Python!
-        return {
-            "comando": "RESPOSTA", 
-            "status": "OK", 
-            "mensagem": f"BEM_VINDO: {self.gerenciador.slots[id_jogador].nick}"
-        }
-    
-    def resposta_nickname(self, id_jogador, dados):
-        nome = dados.get("nome", "SemNome") #var nome vai receber  o q ta no campo "nome" ou vai receber a string "SemNome" se o campo n existir
-        self.gerenciador.slots[id_jogador].nick = nome
-        resposta = self.gera_boas_vindas(id_jogador)
-        broadcast = json.dumps({"comando": "NOVO_JOGADOR", "nome": nome}) + "\n"
-        return resposta, broadcast
-
-#-------------------------------------------------------------------------------------------------
-    def resposta_plantar(self, id_jogador, dados):
-
-        #vou usar pra fazer o broadcast, pra mostrar quem fez a ação, talves mais um campo, mas ai teria que tratar no cliente. 
-        #nick_jogador = self.gerenciador.slots[id_jogador].nick
-
-        broadcast = None
-
-        semente, x, y = dados.get("semente"), dados.get("x"), dados.get("y")
+    def plantar(self, id_jogador, x, y, semente):
+        """O cliente chama passando direto os inteiros x, y e semente"""
         resultado = self.mapa.plantar(x, y, semente)
-        
-        if resultado["validar"] == True:
-            resposta = {"comando": "RESPOSTA", "status": "OK", "mensagem": resultado["motivo"]}
-            novo_valor = self.mapa.matriz[x][y]
-            dados_broadcast = {"comando": "ATUALIZAR_CELULA", "x": x, "y": y, "valor": novo_valor}
-            broadcast = json.dumps(dados_broadcast) + "\n"
-        else:
-            resposta = {"comando": "RESPOSTA", "status": "ERRO", "mensagem": resultado["motivo"]}
-            
-        return resposta, broadcast
+        return resultado 
 
-#-------------------------------------------------------------------------------------------------
-    def resposta_colher(self, id_jogador, dados):
-
-        #vou usar pra fazer o broadcast, pra mostrar quem fez a ação, talves mais um campo, mas ai teria que tratar no cliente. 
-        #nick_jogador = self.gerenciador.slots[id_jogador].nick
-
-        broadcast = None 
-
-        cultura, x, y = dados.get("cultura"), dados.get("x"), dados.get("y")
+    def colher(self, id_jogador, x, y, cultura):
         resultado = self.mapa.colher(x, y, cultura)
-        
-        if resultado["validar"] == True:
-            resposta = {"comando": "RESPOSTA", "status": "OK", "mensagem": resultado["motivo"]}   
-            novo_valor = self.mapa.matriz[x][y]
-            dados_broadcast = {"comando": "ATUALIZAR_CELULA", "x": x, "y": y, "valor": novo_valor}
-            broadcast = json.dumps(dados_broadcast) + "\n"
-        else:
-            resposta = {"comando": "RESPOSTA", "status": "ERRO", "mensagem": resultado["motivo"]}
+        return resultado
 
-        return resposta, broadcast
-
-#-------------------------------------------------------------------------------------------------
-    def resposta_matriz(self):
-        resposta = {"comando": "ATUALIZAR_MAPA", "matriz": self.mapa.matriz}
-        return resposta, None
-
-#-------------------------------------------------------------------------------------------------
-    def resposta_sair(self, id_jogador):
-        nome = self.gerenciador.slots[id_jogador].nick
-        resposta = {"comando": "RESPOSTA", "status": "OK", "mensagem": "Desconectando..."}
-        broadcast = json.dumps({"comando": "JOGADOR_SAIU", "nome": nome}) + "\n"
-        return resposta, broadcast
-
-#-------------------------------------------------------------------------------------------------
-    def resposta_erro(self):
-        resposta = {"comando": "RESPOSTA", "status": "ERRO", "mensagem": "Comando desconhecido"}
-        return resposta, None
-#-------------------------------------------------------------------------------------------------
-
-    def processa_mensagem(self, id_jogador, mensagem_str):
-        try:
-            # print(f"[JOGO] Comando do Jogador {id_jogador}: {mensagem_str}") #comentado para não floodar o terminal
-            try:
-                dados = json.loads(mensagem_str)
-            except json.JSONDecodeError:
-                return json.dumps({"comando": "RESPOSTA", "status": "ERRO", "mensagem": "JSON inválido"}) + "\n", None
-
-            comando_recebido = str(dados.get("comando", "")).strip().upper()
-
-
-            if comando_recebido == "NICKNAME":
-                resposta, broadcast = self.resposta_nickname(id_jogador, dados)
-
-            elif comando_recebido == "PLANTAR":
-                resposta, broadcast = self.resposta_plantar(id_jogador, dados)
-
-            elif comando_recebido == "COLHER":
-                resposta, broadcast = self.resposta_colher(id_jogador, dados)
-
-            elif comando_recebido == "MATRIZ":
-                resposta, broadcast = self.resposta_matriz()
-
-            elif comando_recebido == "SAIR":
-                resposta, broadcast = self.resposta_sair(id_jogador)
-
-            else:
-                resposta, broadcast = self.resposta_erro()
-
-            # Converte a resposta pra String e repassa o broadcast intacto
-            return json.dumps(resposta) + "\n", broadcast
-            
-        except Exception as e:
-            import traceback
-            print("\n" + "="*50)
-            print("ESCUDO ANTI-CRASH ATIVADO NO SERVIDOR")
-            print(f"O jogador {id_jogador} clicou rápido demais e gerou um erro interno!")
-            print(f"JSON que causou a falha: {mensagem_str}")
-            print("--- Detalhes do Erro ---")
-            traceback.print_exc() # Isso vai imprimir a exata linha de código que quebrou!
-            print("="*50 + "\n")
-            
-            # Devolvemos um erro seguro para o Cliente, para a interface dele não desconectar
-            resposta_segura = {
-                "comando": "RESPOSTA", 
-                "status": "ERRO", 
-                "mensagem": "Nao espame clickes, jogue na calma !!!."
-            }
-            return json.dumps(resposta_segura) + "\n", None
+    def pegar_matriz(self):
+        """Método simples para o cliente estado atual do mapa."""
+        return self.mapa.matriz
--- src/server/mainServer.py
+++ src/server/mainServer.py
@@ -1,20 +1,19 @@
 from models import ClassMapa as map
 from models import ClassUser as usManager
 from controllers import ClassControlador as ctrl
-from controllers import ClassServidorTCP as tcpS
+#from controllers import ClassServidorTCP as tcpS
 from views import ClassMapaViewTerminal as mapaV
 import random
-
+import Pyro5.api
 
 if __name__ == "__main__":
-    #print("--- INICIALIZANDO MUNDO ---")
-    mapa_jogo = map.Mapa()
 
-    #aleatorizando mapa 
+    #contruindo mapa aleatorio 
+    mapa_jogo = map.Mapa()
     num_oasis = random.randint(3, 6)
     direcao = random.choice(["horizontal", "vertical"])
     num_nascentes = random.randint(3, 6)
-    prob_areia = random.uniform(0.3, 0.5)  # Probabilidade de cada célula ser areia
+    prob_areia = random.uniform(0.3, 0.5)  #Probabilidade de cada célula perto do rio ser areia
     num_pocas = random.randint(3, 6)
     maxtam_pocas = random.randint(2, 4)
 
@@ -23,17 +22,18 @@ if __name__ == "__main__":
     mapaV.MapaView.exibir_colorido(mapa_jogo.matriz)
 
     gerenciador = usManager.GerenciadorUsers()
-    
-    print("--- CONECTANDO AS CAMADAS ---")
     controlador = ctrl.ControladorFazenda(mapa_jogo, gerenciador)
     
-    # Cria a Rede e injeta os gatilhos(callbacks, funções de resposta) do controlador
-    servidor = tcpS.ServidorTCP(
-        host="localhost", 
-        port=12345,
-        ao_conectar=controlador.conecta_jogador,
-        ao_receber_mensagem=controlador.processa_mensagem,
-        ao_desconectar=controlador.desconecta_jogador
-    )
+    print("--- CONECTANDO AS CAMADAS ---")
     
-    servidor.start()
+    daemon = Pyro5.api.Daemon()#Cria o Daemon, e quem atende as chamadas de rede no Pyro5
+    ns = Pyro5.api.locate_ns()#Busca o Name Server, que é o serviço de diretório do Pyro5
+    uri = daemon.register(controlador)#Registra o nosso controlador no Daemon (ganha um endereço de memória/URI)
+    ns.register("fazenda.servidor", uri) #Anota esse endereço físico na Lista Telefônica com um nome lógico e fácil
+
+    
+    print("Servidor RMI online e registrado como 'fazenda.servidor'")
+    print("Aguardando os métodos serem invocados pelos clientes...")
+    
+ 
+    daemon.requestLoop()#loop infinito escutando as chamadas
